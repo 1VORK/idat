dac1d52751466625827d08dd050db2a4430c739b
IVORK <69377054+1VORK@users.noreply.github.com>
2022-01-07 09:56:15 +1030

    Update idat.py

--- idat.py
+++ idat.py
@@ -9,26 +9,26 @@ async def on_message(m):
         mem = m.mentions[0]
         lvl = int(m.content.split()[18])
         if lvl > 99:
-            await mem.add_role(get(m.guild.roles, id=0))
+            await mem.add_roles(get(m.guild.roles, id=0)) #ant
         elif lvl > 89:
-            await mem.add_role(get(m.guild.roles, id=0))
+            await mem.add_roles(get(m.guild.roles, id=772193415802388500)) #outside
         elif lvl > 79:
-            await mem.add_role(get(m.guild.roles, id=0))
+            await mem.add_roles(get(m.guild.roles, id=772192532401750017)) #spoon
         elif lvl > 69:
-            await mem.add_role(get(m.guild.roles, id=0))
+            await mem.add_roles(get(m.guild.roles, id=609437506714206215)) #crocs
         elif  lvl > 59:
-            await mem.add_role(get(m.guild.roles, id=0))
+            await mem.add_roles(get(m.guild.roles, id=617923744811319317)) #crayf
         elif lvl > 49:
-            await mem.add_role(get(m.guild.roles, id=0))
+            await mem.add_roles(get(m.guild.roles, id=617923740117762088)) #manc
         elif lvl > 39:
-            await mem.add_role(get(m.guild.roles, id=0))
+            await mem.add_roles(get(m.guild.roles, id=609459315916406847)) #bee
         elif lvl > 29:
-            await mem.add_role(get(m.guild.roles, id=0))
+            await mem.add_roles(get(m.guild.roles, id=617923748737187858)) #crab
         elif lvl > 19:
-            await mem.add_role(get(m.guild.roles, id=531217764145430528))
+            await mem.add_roles(get(m.guild.roles, id=531217764145430528)) #hyd
         elif lvl > 9:
-            await mem.add_role(get(m.guild.roles, id=617654956845170701))
+            await mem.add_roles(get(m.guild.roles, id=617654956845170701)) #mag
         elif lvl > 4:
-            await mem.add_role(get(m.guild.roles, id=531214594480406548))
+            await mem.add_roles(get(m.guild.roles, id=531214594480406548)) #frog
 
 client.run(open('tokens/idat','r').read())
